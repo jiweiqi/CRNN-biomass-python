6b470eb214371b82fe27448b31cf1970ce9025d7
fhrichter <64441240+fhrichter@users.noreply.github.com>
2021-05-13 12:01:52 -0700

    Updated and cleaned the plot script

--- Figures/Rate Comparison/rate_constant_graph.py
+++ Figures/Rate Comparison/rate_constant_graph.py
@@ -1,139 +1,87 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar  8 11:50:08 2021
+Created on Thu May 13 09:42:12 2021
 
 @author: Franz Richter
 """
-import math
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import math
+import pandas as pd
 from collections import OrderedDict
+import matplotlib.pyplot as plt
 
+class CRNN:
+    def __init__(self):
 
-def rateconstantcrnn(T):
-
-    rate = []
-    
-    A, E, b = getparameters()
-    
-    for i in range(6):
-        rates = (lambda A = A[i], E = E[i], b = b[i], T = T: 
-                 [math.exp(A) * (x ** b) * math.exp(-E/(8.314*x)) for x in T])()
-        rate.append(rates)
-    
-    return rate
-
-def getparameters():
-    A = [17.3, 14.92, 36.75, 22.69, 14.08, 33.64]
-    E = [222.4, 117.2, 218, 88.7, 110.5, 187.4]
-    E = [x * 1000 for x in E]
-    b = [0, 0.15, 0.34, 0.05, 0.03, 0.04]
-    eps = 1e-9
-    count = 0
-    v = [0.45, 0.33, 0.19]
-    for i in range(3,6):
-        A[i] = A[i] * (eps ** v[count])
-    return [A, E, b]
-    
-
-
-
-def rateconstantburnham(T):
-    def calculaterates(E, A):
-        rate = []
-    
-        for energy in E:
-            for factor in A:
-                rates = (lambda A = factor, E = energy, T = T: 
-                 [(10 ** A) * math.exp(-E/(8.414*x)) for x in T])()
-                rate.append(rates)
-        return rate
+        self.Arrhenius = np.array([[222.4, 0, 17.3, 0],
+                               [117.2, 0.15, 14.92, 0],
+                               [218.0, 0.34, 36.75, 0],
+                               [88.7, 0.05, 22.69, 0.45],
+                               [110.5, 0.03, 14.08, 0.33],
+                               [187.4, 0.04, 33.64, 0.19]])
+        self.lb = 1e-9
+        self.lnO2 = np.log(self.lb)
+        self.R = 8.314E-3
         
-    A = [13, 15]
-    E = [197000, 217000]
-    rate = calculaterates(E, A)
+    def cal_k(self, Ts):
+        lnA = self.Arrhenius[:, 2]
+        b = self.Arrhenius[:, 1]
+        Ea = self.Arrhenius[:, 0]
+        nu = self.Arrhenius[:, 3]
+            
+        rates = []
+        for i in range(6):
+            rates.append(self.get_rates(Ea[i], lnA[i], b[i], nu[i], T))
 
-    min_rate = []
-    max_rate = []
+        return rates
     
-    for pos in range(len(rate[0])):
-        rates_at_pos = (lambda i = pos, rates = rate: [x[i] for x in rate])()
-        min_rate.append(min(rates_at_pos))
-        max_rate.append(max(rates_at_pos))
+    def get_rates(self, E, A, b, nu, T):
+        return (lambda lnA = A, b = b, Ea = E, T = T, R = self.R, nu = nu, lnO2 = self.lnO2:
+                [np.exp(nu * lnO2 + lnA + b*np.log(x) - Ea / (R * x)) for x in T])()
     
-    
-    return [min_rate, max_rate]
-    
-
-
-def rateconstantdauenhauer():
-    data = pd.read_csv("Dauenhauerdata.csv", skiprows = 1)
-    return data
-
-
-def rateconstantantal(T):
-    def caculaterates(E, A, T):
+class LITR:
+    def __init__(self):
+        print("Class started")
+        
+    def cal_burnham_rates(self, T):
+        As = [13, 15]
+        Es = [197, 217]
+        
+        rates =[]
+        for E in Es:
+            for A in As:
+                rates.append(self.cal_rates(E, A, T))
+                
+        
+        return self.get_min_max_rate(rates, T)
+        
+        
+        
+    def cal_rates(self, E, A, T):
         rate = (lambda A = A, E=E*1000, T = T:
                 [(10 ** A) * math.exp(-E/(8.314 * x)) for x in T])()
         return rate
+    
+    def get_min_max_rate(self, rates, T):
+        min_rate = []
+        max_rate = []
+        for pos in range(len(T)):
+            rates_at_pos = (lambda i = pos, rates = rates: [x[i] for x in rates])()
+            min_rate.append(min(rates_at_pos))
+            max_rate.append(max(rates_at_pos))
+        return [min_rate, max_rate]
         
+    def get_dauenhauer_rates(self):
+        return pd.read_csv("Dauenhauerdata.csv", skiprows = 1)
     
-    data = pd.read_csv("Antal.csv")
-    data["rate"] = data.apply(lambda row: caculaterates(row["E"],
-                                        row["log A"], T),
-                            axis = 1)
-    data.drop(labels = 27, axis = 0, inplace = True) #get rid of the outlier
-    data.reset_index(drop=True, inplace=True)
-    
-    # Assemble mean
-    ratesmatrix = []
-    for i in range(len(T)):
-            ratesmatrix.append((lambda xlist = data["rate"], idx = i:
-                           [x[i] for x in xlist])())
-    meanrate = [np.mean(x) for x in ratesmatrix]
-    stdrate = [np.std(x) for x in ratesmatrix]  
-    
-    
-    
-    return [[min(x) for x in ratesmatrix],
-            [max(x) for x in ratesmatrix]]
-
-def plottherates(T, crnn_rates, burnham_rates, dauenhauer_rates, antal_rates):
-    
-    inversetemperature = [1000/x for x in T]
-    
-    
-    #CRNN rates
-    crnn_labels = [f"R{x}" for x in range(1, 7)]
-    names, linestyles = getlinestyles()
-    
-    for i, rate in enumerate(crnn_rates):
-        plt.plot(inversetemperature, rate,"k", linestyle = linestyles[names[i]],
-                 label = crnn_labels[i])
+    def cal_antal_rates(self, T):
+        data = pd.read_csv("Antal.csv")
+        data["rate"] = data.apply(lambda row: self.cal_rates(row["E"], 
+                                                             row["log A"],
+                                                             T), 
+                                  axis = 1)
+        return self.get_min_max_rate(data["rate"], T)
         
-     #Burnham rates
-    plt.fill_between(inversetemperature,burnham_rates[0], burnham_rates[1], 
-                     alpha = 0.1, label = "Burnham et al 2015")
-        
-    
-    #Dauenhauer rates
-    plt.plot(dauenhauer_rates["X"], dauenhauer_rates["Y"], "-o",
-             label = "Cell. Consumption [Krumm et al 2016]")
-    plt.plot(dauenhauer_rates["X.1"], dauenhauer_rates["Y.1"], "-o",
-             label = "Furans Production [Krumm et al 2016]")
-    
-    
-    #Antal
-    plt.fill_between(inversetemperature, antal_rates[0], antal_rates[1], 
-                     alpha = 0.1, label = "Antal et al. 1980 - 2002")
-    
-    plt.yscale("log")
-    plt.legend(loc = "lower left")
-    plt.xlabel("$10^3$/T (K$^{-1}$)")
-    plt.ylabel("Rate Constant AT$^b$exp(-E$_a$/RT)")
-    plt.savefig("figure.png")
-    
 
 def getlinestyles():
     linestyles = OrderedDict(
@@ -164,18 +112,48 @@ def getlinestyles():
         ]
     
     return [names, linestyles]
-    
-
 
 if __name__ == '__main__':
+    
+        # Get Temperatures
+        T = [x + 273 for x in list(range(300, 600))]
+        iT = [1000/x for x in T]
+        
+        # Open classes
+        crnn = CRNN()
+        literature = LITR()
+        
+        #CRNN Rates
+        crnn_labels = [f"R{x}" for x in range(1, 7)]
+        names, linestyles = getlinestyles()
+        for i, rate in enumerate(crnn.cal_k(T)):
+            plt.plot(iT, rate,"k", linestyle = linestyles[names[i]],
+                 label = crnn_labels[i])
+        
+        # Burnhame Rates
+        min_rate, max_rate = literature.cal_burnham_rates(T)
+        plt.fill_between(iT,min_rate, max_rate, 
+                     alpha = 0.1, label = "Burnham et al 2015")
 
-    T = list(range(300, 600)) #temperature
-    T = [x + 273 for x in T]
-    crnn_rates = rateconstantcrnn(T)
-    burnham_rates = rateconstantburnham(T)
-    dauenhauer_rates = rateconstantdauenhauer()
-    antal_rates = rateconstantantal(T)  
+        #Dauenhauer rates
+        dauenhauer_rates = literature.get_dauenhauer_rates()
+        plt.plot(dauenhauer_rates["X"], dauenhauer_rates["Y"], "-o",
+             label = "Cell. Consumption [Krumm et al 2016]")
+        plt.plot(dauenhauer_rates["X.1"], dauenhauer_rates["Y.1"], "-o",
+             label = "Furans Production [Krumm et al 2016]")
 
+        #Antal rates
+        min_rate, max_rate = literature.cal_antal_rates(T)
+        plt.fill_between(iT,min_rate, max_rate, 
+                     alpha = 0.1, label = "Antal et al 1980 - 2002")
+        
+        # Additional information
+        plt.yscale("log")
+        plt.legend(loc = "lower left")
+        plt.xlabel("$10^3$/T (K$^{-1}$)")
+        plt.ylabel("Rate Constant AT$^b$exp(-E$_a$/RT)")
+        plt.savefig("figure.png")
+        
 
-    plottherates(T, crnn_rates, burnham_rates, dauenhauer_rates,
-             antal_rates)
+        
+        
